49c31290fdd0ac08d4ad1bcb9951ff511ca9d830
jiakang <jiakang@staff.sina.com.cn>
2017-05-19 15:41:55 +0800

    added Prints

---
+++ common-code/src/main/java/com/david/common/util/Prints.java
@@ -0,0 +1,16 @@
+package com.david.common.util;
+
+/**
+ * 打印。用于打印相关的封装
+ * Created by jiakang on 2017/5/19.
+ */
+public class Prints {
+
+    public static void print(String str) {
+        System.out.print(str);
+    }
+
+    public static void println(String str) {
+        System.out.println(str);
+    }
+}
